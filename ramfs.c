565c05d969c73dcc82750e9fe36fbcf04954a219
xingjunyang <xingjunyang@smail.nju.edu.cn>
2023-01-05 13:04:57 +0800

    Reformatted code.
    Signed-off-by: Junyang Xing <1057067557@qq.com>

--- ramfs.c
+++ ramfs.c
@@ -1,38 +1,39 @@
 #include "ramfs.h"
+
 /* modify this file freely */
 
 int ropen(const char *pathname, int flags) {
-  // TODO();
+    // TODO();
 }
 
 int rclose(int fd) {
-  // TODO();
+    // TODO();
 }
 
 ssize_t rwrite(int fd, const void *buf, size_t count) {
-  // TODO();
+    // TODO();
 }
 
 ssize_t rread(int fd, void *buf, size_t count) {
-  // TODO();
+    // TODO();
 }
 
 off_t rseek(int fd, off_t offset, int whence) {
-  // TODO();
+    // TODO();
 }
 
 int rmkdir(const char *pathname) {
-  // TODO();
+    // TODO();
 }
 
 int rrmdir(const char *pathname) {
-  // TODO();
+    // TODO();
 }
 
 int runlink(const char *pathname) {
-  // TODO();
+    // TODO();
 }
 
 void init_ramfs() {
-  // TODO();
+    // TODO();
 }
--- ramfs.h
+++ ramfs.h
@@ -16,11 +16,19 @@ typedef uint64_t size_t;
 typedef int64_t off_t;
 
 int ropen(const char *pathname, int flags);
+
 int rclose(int fd);
+
 ssize_t rwrite(int fd, const void *buf, size_t count);
+
 ssize_t rread(int fd, void *buf, size_t count);
+
 off_t rseek(int fd, off_t offset, int whence);
+
 int rmkdir(const char *pathname);
+
 int rrmdir(const char *pathname);
+
 int runlink(const char *pathname);
+
 void init_ramfs();
