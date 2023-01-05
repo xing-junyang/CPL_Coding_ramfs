a309f374ffdd1bd5208805dc0065e1d1bc979bdc
xingjunyang <xingjunyang@smail.nju.edu.cn>
2023-01-05 20:57:24 +0800

    Adding path analysis and destroyer.
    Signed-off-by: Junyang Xing <1057067557@qq.com>

--- ramfs.c
+++ ramfs.c
@@ -1,9 +1,10 @@
 #include "ramfs.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-/* modify this file freely */
-
-typedef struct File{
+/* To store all file and directories */
+typedef struct File {
     /* Common properties */
     bool isDirectory;
     char *fileName;
@@ -13,7 +14,80 @@ typedef struct File{
     /* File properties */
     int offset;
     struct File *nextFile;
-}file;
+} file;
+
+typedef struct Path {
+    enum {
+        FILE, DIRECTORY, ERROR
+    } pathType;
+    char **name;
+    int nameCount;
+} path;
+
+bool checkNameValidity(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (!(isdigit(str[i]) || isalpha(str[i]) || str[i] == '.')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void destroyPath(path src) {
+    for (int i = 0; i < src.nameCount; i++) {
+        free(src.name[i]);
+    }
+    free(src.name);
+}
+
+path analyzePath(const char *pathname) {
+    path ret;
+    int len = (int) strlen(pathname);
+    if (pathname[0] != '/') {
+        ret.pathType = ERROR;
+        return ret;
+    }
+
+    ret.name = malloc(sizeof(*ret.name) * 1024);
+    ret.nameCount = 0;
+
+    if (pathname[len - 1] == '/') {
+        ret.pathType = DIRECTORY;
+    } else {
+        ret.pathType = FILE;
+    }
+
+    char *tmp = malloc(sizeof(char) * len);
+    memset(tmp, 0, sizeof(*tmp));
+    int tmpLen = 0;
+    bool slashRead = 0;
+
+    for (int i = 0; i <= len; i++) {
+        if (pathname[i] == '/' && (!slashRead)) {
+            slashRead = 1;
+            if (checkNameValidity(tmp) && tmpLen) {
+                ret.name[ret.nameCount++] = malloc(sizeof(char) * 1024);
+                strcpy(ret.name[ret.nameCount], tmp);
+                memset(tmp, 0, sizeof(*tmp));
+                tmpLen = 0;
+            } else if (tmpLen) {
+                ret.pathType = ERROR;
+                free(tmp);
+                return ret;
+            }
+        } else if (pathname[i] == '/' && slashRead) {
+            continue;
+        } else {
+            slashRead = 0;
+            tmp[tmpLen++] = pathname[i];
+        }
+    }
+
+    free(tmp);
+    return ret;
+}
+
 
 int ropen(const char *pathname, int flags) {
     // TODO();
@@ -49,4 +123,12 @@ int runlink(const char *pathname) {
 
 void init_ramfs() {
     // TODO();
+    /* Initializing root directory */
+    file *root = malloc(sizeof(*root));
+    root->isDirectory = 1;
+    root->fileName = NULL;
+    root->nextFile = NULL;
+    root->haveChild = 0;
+    root->firstChildFile = NULL;
+
 }
